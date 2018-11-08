689b5c78f06ed73c1f34f22bca948448e6879939
ibeyerlein <ingo.beyerlein@allmaennitta.de>
2018-11-08 03:20:25 +0100

    make patricia trie transparent by logging

--- src/test/scala/de/allmaennitta/examples/_46_OwnCollections_PatriciaTrie.scala
+++ src/test/scala/de/allmaennitta/examples/_46_OwnCollections_PatriciaTrie.scala
@@ -19,9 +19,21 @@ class _46_OwnCollections_PatriciaTrie extends FunSpec with Matchers {
     var suffixes: immutable.Map[Char, PrefixMap[T]] = Map.empty
     var value: Option[T] = None
 
-    def get(s: String): Option[T] =
-      if (s.isEmpty) value
-      else suffixes get s(0) flatMap (_.get(s substring 1))
+    def get(s: String): Option[T] = {
+      println("s:"+s)
+      if (s.isEmpty) {
+        println("GET: s empty => :"+value.toString)
+        value
+      }
+      else {
+//        suffixes get s(0) flatMap (_.get(s substring 1))
+        val s0 = suffixes get s(0)
+        println("GET: s0:"+s0)
+        val fmap = s0 flatMap (_.get(s substring 1))
+        println("GET: fmap:"+fmap)
+        fmap
+      }
+    }
 
     def withPrefix(s: String): PrefixMap[T] =
       if (s.isEmpty) this
@@ -30,13 +42,19 @@ class _46_OwnCollections_PatriciaTrie extends FunSpec with Matchers {
         suffixes get leading match {
           case None =>
             suffixes = suffixes + (leading -> empty)
+            println("WITH_P: case None: "+suffixes)
           case _ =>
+            println("WITH_P: case Hit: "+suffixes)
         }
-        suffixes(leading) withPrefix (s substring 1)
+        val suf = suffixes(leading) withPrefix (s substring 1)
+        println("WITH_P: result: "+suf)
+        suf
       }
 
-    override def update(s: String, elem: T) =
+    override def update(s: String, elem: T) = {
+      println("UPDATE: Der Wert von "+withPrefix(s)+ " wird auf "+elem.toString+" gesetzt.")
       withPrefix(s).value = Some(elem)
+    }
 
     override def remove(s: String): Option[T] =
       if (s.isEmpty) {
@@ -80,34 +98,70 @@ class _46_OwnCollections_PatriciaTrie extends FunSpec with Matchers {
   }
 
   describe("A PrefixMap") {
-    it("can be created") {
+    it("can be constructed") {
       var m1 = PrefixMap.empty[Int]
-      m1.size should be (0)                                // empty
-      val retVal = m1 += ("abc" -> 5)                      // +=
-      retVal should be (PrefixMap("abc" -> 5))             // apply
-      m1 should be (PrefixMap("abc" -> 5))
-      m1 map { case (k,v) => (k+"!", v+1)} should be       // canBuildFrom
-        (PrefixMap("abc!" -> 6))
+      m1.size should be(0) // empty
+      val retVal = m1 += ("abc" -> 5) // +=
+      retVal should be(PrefixMap("abc" -> 5)) // apply
+      m1 should be(PrefixMap("abc" -> 5))
+      m1 map { case (k, v) => (k + "!", v + 1) } should be // canBuildFrom
+      (PrefixMap("abc!" -> 6))
       val concatMap = m1 ++= PrefixMap("xyz" -> 11)
-      concatMap  should be (PrefixMap("abc" -> 5, "xyz" -> 11))
-      val capitals = PrefixMap("Andorra"->"Andorra la Vella", "Angola"->"Luanda", "Antigua und " +
-        "Barbuda"->"St. John’s", "Äquatorialguinea"->"Malago", "Argentinien" ->"	Buenos Aires",
-        "Armenien"->"Jerewan", "TestCountry"->"TestCapital")
-      capitals.get("TestCountry") should be (Some("TestCapital"))
-      capitals.remove("TestCountry")                                              //remove
-      capitals.get("TestCountry") should be (None)                                //get
-      capitals.update("Äquatorialguinea","Malabo")                                //update
-      capitals.withPrefix("An") should be (PrefixMap(                             //withPrefix
-        "dorra" -> "Andorra la Vella", "gola" -> "Luanda", "tigua und Barbuda" -> "St. John’s"))
+      concatMap should be(PrefixMap("abc" -> 5, "xyz" -> 11))
+    }
+     it("has items removed and updated") {
+       val capitals = PrefixMap("Andorra" -> "Andorra la Vella", "Angola" -> "Luanda", "Antigua und " +
+         "Barbuda" -> "St. John’s", "Äquatorialguinea" -> "Malago",
+         "Argentinien" -> "Buenos Aires", "Armenien" -> "Jerewan", "TestCountry" -> "TestCapital")
+       capitals.get("TestCountry") should be(Some("TestCapital"))
+       capitals.remove("TestCountry") //remove
+       capitals.get("TestCountry") should be(None) //get
+       capitals.update("Äquatorialguinea", "Malabo") //update
+       capitals.get("Äquatorialguinea") should be(Some("Malabo"))
+     }
+    it("has items retrieved by 'get'") {
+      println("INIT_MAP_START")
+      val capitals = PrefixMap("Andorra" -> "Andorra la Vella", "Angola" -> "Luanda", "Antigua und " +
+        "Barbuda" -> "St. John’s", "Äquatorialguinea" -> "Malabo", "Argentinien" ->
+        "Buenos Aires", "Armenien" -> "Jerewan")
+      println("INIT_MAP_END")
+      println("GET_START")
+      val aequ = capitals.get("Äquatorialguinea")
+      println("GET_END")
+      println("EVALUATION_START")
+      aequ should be(Some("Malabo")) //get
+      println("EVALUATION_END")
+    }
+    it("has items retrieved by 'withPrefix'") {
+      println("INIT_MAP_START")
+      val capitals = PrefixMap("Andorra" -> "Andorra la Vella", "Angola" -> "Luanda", "Antigua und " +
+        "Barbuda" -> "St. John’s", "Äquatorialguinea" -> "Malabo", "Argentinien" ->
+        "Buenos Aires", "Armenien" -> "Jerewan")
+      println("INIT_MAP_END")
+      println("WITH_PREFIX_START")
+      val an = capitals.withPrefix("An")
+      println("WITH_PREFIX_END")
+      println("EVALUATION_START")
+      an should be (PrefixMap("dorra" -> "Andorra la Vella", "gola" -> "Luanda",  //withPrefix
+        "tigua und Barbuda" -> "St. John’s"))
+      println("EVALUATION_END")
+    }
 
-//      m.get("hi").getOrElse(-99) should be (2)                                  // => apply
-//      m.get("h").getOrElse(-99) should be (-99)                                  // => apply
-//      m map { case (k, v) => (k + "!", "x" * v) } should be
-//        (PrefixMap("hello!" -> "xxxxx", "hi!" -> "xx" ))                        // => canBuildFrom
-
-//      m2.get("xy").getOrElse(-99) should be (4)
-//      val m3 = m2 withPrefix("x")
-//      m3 should be (PrefixMap("y" -> 4))
+    it("is built up incrementally") {
+      println("EMPTY")
+      var p = PrefixMap.empty[Int]
+      println("+AB")
+      p = p+=("AB"->4)
+      println("Map: "+p)
+      println("Suffixes: "+p.suffixes)
+      println("+ABA")
+      p = p+=("ABA"->5)
+      println("Map: "+p)
+      println("Suffixes: "+p.suffixes)
+      println("+ABI")
+      p = p+=("ABI"->6)
+      println("Map: "+p)
+      println("Suffixes: "+p.suffixes)
     }
   }
 
