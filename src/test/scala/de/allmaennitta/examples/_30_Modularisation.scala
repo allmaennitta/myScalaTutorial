ea2af36adf8fa64e523e4d6a02be9da913e12bc3
dictyo <ingo.beyerlein@allmaennitta.de>
2018-11-10 22:21:11 +0100

    finished modularisation

--- src/test/scala/de/allmaennitta/examples/_30_Modularisation.scala
+++ src/test/scala/de/allmaennitta/examples/_30_Modularisation.scala
@@ -1,10 +1,6 @@
 package de.allmaennitta.examples
 
-import java.io.StringReader
-
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import de.allmaennitta.examples.yaml.Sample
+import de.allmaennitta.examples.StudentDatabase.FrozenFood
 import org.scalatest.{FunSpec, Matchers}
 
 abstract class Food(val name: String) {
@@ -27,19 +23,22 @@ object Cream extends Food("Cream")
 
 object Sugar extends Food("Sugar")
 
-trait FoodCategories{ //push modularisation further by separating this topic
-  case class FoodCategory(name: String, foods: List[Food])
-  def allCategories : List[FoodCategory]
+case class FoodCategory(name: String, foods: List[Food]);
+
+trait FoodCategories { //push modularisation further by separating this topic
+  def allCategories: List[FoodCategory]
 }
 
 abstract class Database extends FoodCategories { //common attributes, etc. go here
   def allFoods: List[Food]
+
   def foodNamed(name: String): Option[Food] =
     allFoods.find(f => f.name == name)
+
   def allRecipes: List[Recipe]
 }
 
-object SimpleDatabase extends Database with SimpleFoods with SimpleRecipes{
+object SimpleDatabase extends Database with SimpleFoods with SimpleRecipes {
   private var categories = List(
     FoodCategory("fruits", List(Apple, Orange)),
     FoodCategory("misc", List(Cream, Sugar))
@@ -48,12 +47,14 @@ object SimpleDatabase extends Database with SimpleFoods with SimpleRecipes{
   def allCategories = categories
 }
 
-trait SimpleFoods{
+trait SimpleFoods {
+
   object Pear extends Food("Pear")
+
   def allFoods = List(Apple, Orange, Cream, Sugar)
 }
 
-trait SimpleRecipes{
+trait SimpleRecipes {
   this: SimpleFoods => //necessary to make pear from SimpleFoods visible
 
   object FruitSalad extends Recipe(
@@ -61,11 +62,14 @@ trait SimpleRecipes{
     List(Apple, Orange, Cream, Sugar, Pear),
     "Stir it all together."
   )
-  def allRecipes= List(FruitSalad)
+
+  def allRecipes = List(FruitSalad)
 }
 
-object StudentDatabase extends Database{
+object StudentDatabase extends Database {
+
   object FrozenFood extends Food("FrozenFood")
+
   object HeatItUp extends Recipe(
     "heat it up",
     List(FrozenFood),
@@ -73,36 +77,41 @@ object StudentDatabase extends Database{
   )
 
   override def allFoods: List[Food] = List(FrozenFood)
+
   override def allRecipes: List[Recipe] = List(HeatItUp)
-  override def allCategories: List[StudentDatabase.FoodCategory] = List(FoodCategory("edible",
+
+  override def allCategories: List[FoodCategory] = List(FoodCategory("edible",
     List(FrozenFood)))
 }
 
 abstract class Browser { //common attributes, etc. go here
   val database: Database
+
   def recipesUsing(food: Food) =
     database.allRecipes.filter(recipe => //hard link! XXX
       recipe.ingredients.contains(food))
 
-  def displayCategory(category: database.FoodCategory)={
+  def displayCategory(category: FoodCategory) = {
     println(category)
   }
 }
 
-object SimpleBrowser extends Browser{
-  val database = SimpleDatabase
-}
-
-object StudentBrowser extends Browser{
-  val database = StudentDatabase
-}
-
 class _30_Modularisation extends FunSpec with Matchers {
-  describe("") {
-    it("") {
+  describe("Both, a wired modularised database and browser") {
+    val db: Database = SimpleDatabase
+    object browser extends Browser {
+      val database: db.type = db //db.type => singleton-type, tell the compiler, that both types are the same object
+                                 //not necessary in this case, but it might be in other situations
+    }
 
+    it("works") {
+      val apple = db.foodNamed("Apple").get
+      for (recipe <- browser.recipesUsing(apple))
+        println(recipe)
+    }
+    it("makes it possible to have same kind of object differently ") {
+      StudentDatabase.allCategories should be(List(FoodCategory("edible", List(FrozenFood))))
     }
   }
-
 }
 
