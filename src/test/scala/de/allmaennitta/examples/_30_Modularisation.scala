58574e12ec16678d68249060a87e2ffa5e7b69ba
ibeyerlein <ingo.beyerlein@allmaennitta.de>
2018-11-10 18:33:30 +0100

    various

--- src/test/scala/de/allmaennitta/examples/_30_Modularisation.scala
+++ src/test/scala/de/allmaennitta/examples/_30_Modularisation.scala
@@ -27,26 +27,19 @@ object Cream extends Food("Cream")
 
 object Sugar extends Food("Sugar")
 
-object FruitSalad extends Recipe(
-  "fruit salad",
-  List(Apple, Orange, Cream, Sugar),
-  "Stir it all together."
-)
+trait FoodCategories{ //push modularisation further by separating this topic
+  case class FoodCategory(name: String, foods: List[Food])
+  def allCategories : List[FoodCategory]
+}
 
-abstract class Database {
+abstract class Database extends FoodCategories { //common attributes, etc. go here
   def allFoods: List[Food]
   def foodNamed(name: String): Option[Food] =
     allFoods.find(f => f.name == name)
   def allRecipes: List[Recipe]
-  case class FoodCategory(name: String, foods: List[Food])
-
-  def allCategories : List[FoodCategory]
 }
 
-object SimpleDatabase extends Database {
-  def allFoods = List(Apple, Orange, Cream, Sugar)
-  def allRecipes: List[Recipe] = List(FruitSalad)
-
+object SimpleDatabase extends Database with SimpleFoods with SimpleRecipes{
   private var categories = List(
     FoodCategory("fruits", List(Apple, Orange)),
     FoodCategory("misc", List(Cream, Sugar))
@@ -55,7 +48,37 @@ object SimpleDatabase extends Database {
   def allCategories = categories
 }
 
-abstract class Browser {
+trait SimpleFoods{
+  object Pear extends Food("Pear")
+  def allFoods = List(Apple, Orange, Cream, Sugar)
+}
+
+trait SimpleRecipes{
+  this: SimpleFoods => //necessary to make pear from SimpleFoods visible
+
+  object FruitSalad extends Recipe(
+    "fruit salad",
+    List(Apple, Orange, Cream, Sugar, Pear),
+    "Stir it all together."
+  )
+  def allRecipes= List(FruitSalad)
+}
+
+object StudentDatabase extends Database{
+  object FrozenFood extends Food("FrozenFood")
+  object HeatItUp extends Recipe(
+    "heat it up",
+    List(FrozenFood),
+    "Microwave the 'food' for 10 minutes."
+  )
+
+  override def allFoods: List[Food] = List(FrozenFood)
+  override def allRecipes: List[Recipe] = List(HeatItUp)
+  override def allCategories: List[StudentDatabase.FoodCategory] = List(FoodCategory("edible",
+    List(FrozenFood)))
+}
+
+abstract class Browser { //common attributes, etc. go here
   val database: Database
   def recipesUsing(food: Food) =
     database.allRecipes.filter(recipe => //hard link! XXX
@@ -66,9 +89,12 @@ abstract class Browser {
   }
 }
 
-
 object SimpleBrowser extends Browser{
   val database = SimpleDatabase
+}
+
+object StudentBrowser extends Browser{
+  val database = StudentDatabase
 }
 
 class _30_Modularisation extends FunSpec with Matchers {
---
+++ src/test/scala/de/allmaennitta/examples/_55_TODO_Play.scala
@@ -0,0 +1,8 @@
+package de.allmaennitta.examples
+
+import org.scalatest.{BeforeAndAfter, FunSpec, Matchers}
+
+class _55_TODO_Play extends FunSpec with Matchers with BeforeAndAfter  {
+}
+
+
